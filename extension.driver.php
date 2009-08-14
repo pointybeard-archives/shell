5d07ae20b1076476974e01535f620c63086537c4
Alistair Kearney <alistair@21degrees.com.au>
2009-08-14 15:46:32 +1000

    Updated change log and version

--- extension.driver.php
+++ extension.driver.php
@@ -4,8 +4,8 @@
 
 		public function about(){
 			return array('name' => 'Shell',
-						 'version' => '0.1',
-						 'release-date' => '2009-08-13',
+						 'version' => '0.2',
+						 'release-date' => '2009-08-14',
 						 'author' => array('name' => 'Alistair Kearney',
 										   'website' => 'http://symphony-cms.com',
 										   'email' => 'alistair@symphony-cms.com')
