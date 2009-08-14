1dfbab6cd1f1c5896fd2d8eefcd2204b8fbd4b3a
Alistair Kearney <alistair@21degrees.com.au>
2009-08-14 15:45:08 +1000

    Tweaked run() function signature. Added usage() function

--- lib/class.shellcommand.php
+++ lib/class.shellcommand.php
@@ -1,5 +1,8 @@
 <?php
 	
 	Abstract Class ShellCommand{
-		abstract public function run();
+		abstract public function run(array $args=NULL);
+		public function usage(){
+			echo "No usage information for this command is available.";
+		}
 	}
