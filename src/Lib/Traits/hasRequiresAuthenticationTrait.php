f5acd456f130f102baa2f73178484f1e6477bebf
pointybeard <hi@alistairkearney.com>
2016-07-03 12:35:47 +1000

    The hasRequiresAuthenticationTrait trait will now prompt for username and password if none was provided.

--- src/Lib/Traits/hasRequiresAuthenticationTrait.php
+++ src/Lib/Traits/hasRequiresAuthenticationTrait.php
@@ -9,9 +9,30 @@ use Symphony\Shell\Lib;
  */
 trait hasRequiresAuthenticationTrait
 {
-    function authenticate() {
-        if(!Lib\Shell::instance()->isLoggedIn()){
-            throw new Lib\Exceptions\AuthenticationRequiredException;
+    public function authenticate()
+    {
+        if (!Lib\Shell::instance()->isLoggedIn()) {
+
+            // Let the user know they are not authenticated.
+            (new Lib\Message("NOTICE: This command requires authentication. Please provide credentials below."))
+                ->foreground("yellow")
+                ->prependDate(false)
+                ->appendNewLine(true)
+                ->display()
+            ;
+
+            // If we got this far, it means the user did not provide the
+            // -u or -t argments. We should still give them an opportunity
+            // to provide authentication.
+            $username = Lib\Utils::promptForInput("username");
+            $password = Lib\Utils::promptForInput("password", true);
+
+            // Try to log the user in with the credential supplied.
+            if (false == Lib\Shell::instance()->login($username, $password)) {
+                throw new Lib\Exceptions\AuthenticationRequiredException(
+                    "Username and/or password supplied are invalid."
+                );
+            }
         }
     }
 }
