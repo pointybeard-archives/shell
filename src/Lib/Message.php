b759a3a4d87d20e2f5ffb5000c2b5ea83a55db6f
Alannah Kearney <alannah@ruleandmake.com>
2018-10-14 11:02:37 +0000

    Ensuring PSR2 compliant formatting. Added __toString() method and moved some code from display().

--- src/Lib/Message.php
+++ src/Lib/Message.php
@@ -13,26 +13,25 @@ class Message
     // Credit to JR from http://www.if-not-true-then-false.com/
     // for the code reponsible for colourising the messages
     private static $foregroundColours = [
-        'black' => '0;30',
-        'dark gray' => '1;30',
-        'blue' => '0;34',
-        'light blue' => '1;34',
-        'green' => '0;32',
-        'light green' => '1;32',
-        'cyan' => '0;36',
-        'light cyan' => '1;36',
-        'red' => '0;31',
-        'light red' => '1;31',
-        'purple' => '0;35',
-        'light purple' => '1;35',
-        'brown' => '0;33',
-        'yellow' => '1;33',
-        'light gray' => '0;37',
-        'white' => '1;37',
+        'black'         => '0;30',
+        'red'           => '0;31',
+        'green'         => '0;32',
+        'brown'         => '0;33',
+        'blue'          => '0;34',
+        'purple'        => '0;35',
+        'cyan'          => '0;36',
+        'white'         => '1;37',
+        'dark gray'     => '1;30',
+        'light red'     => '1;31',
+        'light green'   => '1;32',
+        'yellow'        => '1;33',
+        'light blue'    => '1;34',
+        'light purple'  => '1;35',
+        'light cyan'    => '1;36',
+        'light gray'    => '0;37',
     ];
 
     private static $backgroundColours = [
-        'default'       => '49',
         'black'         => '40',
         'red'           => '41',
         'green'         => '42',
@@ -40,15 +39,16 @@ class Message
         'blue'          => '44',
         'magenta'       => '45',
         'cyan'          => '46',
+        'default'       => '49',
+        'white'         => '107',
         'light gray'    => '47',
-        'dark gray'     => '100',
         'light red'     => '101',
         'light green'   => '102',
         'light yellow'  => '103',
         'light blue'    => '104',
         'light magenta' => '105',
         'light cyan'    => '106',
-        'white'         => '107',
+        'dark gray'     => '100',
     ];
 
     public function __get($name)
@@ -108,6 +108,11 @@ class Message
 
     public function display($target=STDOUT)
     {
+        return fputs($target, (string)$this);
+    }
+
+    public function __toString()
+    {
         $message = null;
 
         if (!is_null($this->foreground)) {
@@ -121,12 +126,11 @@ class Message
         // Add string and end coloring
         $message .=  $this->message . "\033[0m";
 
-        return fputs($target,
-                sprintf('%s%s%s',
-                    ($this->prependDate ? gmdate($this->dateFormat) : null),
-                    $message,
-                    ($this->appendNewLine ? PHP_EOL : null)
-                )
+        return sprintf(
+            '%s%s%s',
+            ($this->prependDate ? gmdate($this->dateFormat) : null),
+            (string)$message,
+            ($this->appendNewLine ? PHP_EOL : null)
         );
     }
 }
